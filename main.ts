f88aec70329ce68e791ba3e634fd10efeddaf89f
cbpeters007 <94552018+cbpeters007@users.noreply.github.com>
2021-11-18 16:47:26 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
+bluetooth.onBluetoothConnected(function () {
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        `)
+})
+bluetooth.startUartService()
+bluetooth.setTransmitPower(7)
 basic.forever(function () {
 	
 })
